a581af7c23bcaaabf749006909a9997022274f81
JavierMarinGarcia <javier.marin.garcia.linkedin01@hotmail.com>
2023-07-17 14:42:47 -0600

    Contextures Here

---
+++ WebApi/Controllers/ProductoController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Core.Entities;
+using Core.Interfaces;
+using Core.Specifications;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebApi.Dtos;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        private readonly IGenericRepository<Producto> _productoRepository;
+        private readonly IMapper _mapper;
+        public ProductoController(IGenericRepository<Producto> productoRepository, IMapper mapper)
+        {
+            _productoRepository = productoRepository;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Producto>>> GetProductos()
+        {
+            var spec = new ProductoWithCategoriaAndMarcaSpecification();
+            var productos = await _productoRepository.GetAllWithSpec(spec);
+            return Ok(_mapper.Map<IReadOnlyList<Producto>, IReadOnlyList<ProductoDto>>(productos));
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductoDto>> GetProducto(int id)
+        {
+            var spec = new ProductoWithCategoriaAndMarcaSpecification(id);
+            var producto = await _productoRepository.GetByIdWithSpec(spec);
+            return _mapper.Map<Producto, ProductoDto>(producto);
+        }
+    }
+}
---
+++ WebApi/Dtos/MappingProfiles.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Core.Entities;
+
+namespace WebApi.Dtos
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Producto, ProductoDto>();
+        }
+    }
+}
